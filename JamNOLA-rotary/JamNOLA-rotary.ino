8cf370cf6db79c3c9c08a4411d8e9fe9d8e9f4c2
Tully <tullo-x86@users.noreply.github.com>
2025-03-15 20:11:36 -0500

    Make ethernet work

--- JamNOLA-rotary/JamNOLA-rotary.ino
+++ JamNOLA-rotary/JamNOLA-rotary.ino
@@ -5,6 +5,9 @@
  *   - "OSC" by Adrian Freed (Open Sound Control). **Search for "open sound control". This library will not be found when searching "OSC" in the Arduino IDE**
  */
 
+// NOTE: uncomment this line for serial debugging
+//#define ROTARY_DECODER_DEBUG
+
 #ifndef ETH_PHY_TYPE
 #define ETH_PHY_TYPE  ETH_PHY_LAN8720
 #define ETH_PHY_ADDR  0
@@ -21,7 +24,7 @@
 static bool eth_connected = false;
 
 WiFiUDP udp;                                // A UDP instance to let us send and receive packets over UDP
-const IPAddress remoteIp(192,168,1,165);    // remote IP of your computer
+const IPAddress remoteIp(10,101,1,101);     // remote IP of your computer
 const unsigned int remotePort = 9999;       // remote port to receive OSC
 const unsigned int localPort = 8888;        // local port to listen for OSC packets (actually not used for sending)
 
@@ -31,27 +34,27 @@ const unsigned int localPort = 8888;        // local port to listen for OSC pack
 void onEvent(arduino_event_id_t event) {
   switch (event) {
     case ARDUINO_EVENT_ETH_START:
-      Serial.println("ETH Started");
+      Serial.println("ETH: Started");
       // The hostname must be set after the interface is started, but needs
       // to be set before DHCP, so set it from the event handler thread.
       ETH.setHostname("esp32-ethernet");
       break;
     case ARDUINO_EVENT_ETH_CONNECTED: Serial.println("ETH Connected"); break;
     case ARDUINO_EVENT_ETH_GOT_IP:
-      Serial.println("ETH Got IP");
+      Serial.println("ETH: Got IP");
       Serial.println(ETH);
       eth_connected = true;
       break;
     case ARDUINO_EVENT_ETH_LOST_IP:
-      Serial.println("ETH Lost IP");
+      Serial.println("ETH: Lost IP");
       eth_connected = false;
       break;
     case ARDUINO_EVENT_ETH_DISCONNECTED:
-      Serial.println("ETH Disconnected");
+      Serial.println("ETH: Disconnected");
       eth_connected = false;
       break;
     case ARDUINO_EVENT_ETH_STOP:
-      Serial.println("ETH Stopped");
+      Serial.println("ETH: Stopped");
       eth_connected = false;
       break;
     default: break;
@@ -102,12 +105,13 @@ void loop() {
   int32_t numPinsActive = 0;
   int32_t knownSwitchPosition = 7;
 
-
+#ifdef ROTARY_DECODER_DEBUG
   Serial.print("Previous frame:");
   printValues(lastLineValues);
   Serial.println();
 
   Serial.print("This frame:");
+#endif
 
   for (int32_t i = 0; i < positionLineCount; i++) {
     bool thisPin = (digitalRead(positionLinePins[i]) == LOW);
@@ -117,8 +121,10 @@ void loop() {
     if (thisPin) {
       numPinsActive += 1;
       knownSwitchPosition = i;
+#ifdef ROTARY_DECODER_DEBUG
       Serial.print(" ");
       Serial.print(i);
+#endif
     }
 
     if (lastLineValues[i] != currLineValues[i]) {
@@ -126,20 +132,22 @@ void loop() {
     }
   }
 
+#ifdef ROTARY_DECODER_DEBUG
   if (numPinsActive > 0) {
     Serial.println(".");
   }
   else {
     Serial.println(" none.");
   }
+#endif
 
   // If there's been a change and any valid number of pins is being pulled low, send a message
   if (isAnyChange && numPinsActive < 2) {
-    Serial.print("Known switch position: ");
+    Serial.print("Sending switch position: ");
     Serial.println(knownSwitchPosition);
     sendMsg(knownSwitchPosition);
   }
 
-  delay(100);
+  delay(50);
 }
 
